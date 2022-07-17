7db49d87ae3f38c048854dd0204a5b5beecbc9cb
Yang <darker66678@gmail.com>
2022-07-17 14:04:40 +0800

    feat: Add feature selection

---
+++ feature_selection.py
@@ -0,0 +1,32 @@
+from sklearn import feature_selection
+import logging
+import matplotlib.pyplot as plt
+
+
+def plot_bar(select_type, res, X, y, file):
+    bar_X = list(X.columns)
+    plt.figure()
+    plt.bar(bar_X, res)
+    plt.title(f'Data: {file}, Method:{select_type}, Target:{y.name}')
+    plt.savefig(f'./feature_score/{file}-{select_type}.png')
+    print("save bar chart in ./feature_score")
+
+
+def feat_score(select_type, task_type, y, X, file):
+    if(select_type == "chi2"):
+        res = feature_selection.chi2(X, y)[0]
+    elif(select_type == "pearson"):
+        res = feature_selection.r_regression(X, y)
+    elif(select_type == "ANOVA"):
+        res = feature_selection.f_classif(X, y)[0]
+    elif(select_type == "MIC"):
+        if(task_type == "classification"):
+            res = feature_selection.mutual_info_classif(X, y)
+        elif(task_type == "regression"):
+            res = feature_selection.mutual_info_regression(X, y)
+        else:
+            print("unknown task type", task_type)
+            return
+    print(list(X.columns))
+    print(res)
+    plot_bar(select_type, res, X, y, file)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 from funcitons import *
 import datetime
 import os
-
+from feature_selection import *
 
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 for _ in logging.root.manager.loggerDict:
@@ -23,6 +23,11 @@ if __name__ == '__main__':
         "--k_fold", help="set k value , need to >1", default=5, type=int)
     parser.add_argument(
         "--confusion_m", help="Do you need to gernerate the confusion_matrix?(False or True)", default=False, type=bool)
+    parser.add_argument(
+        "--feat_select", help="feature selection", default=False, type=bool)
+    parser.add_argument(
+        "--feat_select_type", help="chi2, pearson, ANOVA, MIC ", default="chi2", type=str)
+
     args = parser.parse_args()
 
     with open('./dataset/custom.json') as f:
@@ -30,29 +35,33 @@ if __name__ == '__main__':
     dataset, task_type, y, X, file, target, data_path = load_data(
         args.data, custom_data)
 
-    model_cfg = load_ML_model_cfg(args)
+    if(args.feat_select == True):
+        feat_score(args.feat_select_type, task_type, y, X, file)
 
-    rightnow = str(datetime.datetime.today()).replace(
-        " ", "_").replace(":", "-")[:-7]
-    folder = f'./results/{rightnow}_{file}_{args.model}_{args.algo}'
-    os.makedirs(folder)
-    log_path = f'{folder}/{args.algo}({args.model})_{args.data}.log'
-    logging.basicConfig(
-        level=logging.DEBUG, filename=log_path, filemode='w', format=FORMAT)
+    else:
+        model_cfg = load_ML_model_cfg(args)
 
-    print(
-        f'Algo: {args.algo}, Model: {args.model}, Data: {args.data}, scoring: {args.scoring}, k_fold: {args.k_fold}, confusion_m: {args.confusion_m}')
-    logging.info(
-        f'Algo: {args.algo}, Model: {args.model}, Data: {args.data}, scoring: {args.scoring}, k_fold: {args.k_fold}, confusion_m: {args.confusion_m}')
+        rightnow = str(datetime.datetime.today()).replace(
+            " ", "_").replace(":", "-")[:-7]
+        folder = f'./results/{rightnow}_{file}_{args.model}_{args.algo}'
+        os.makedirs(folder)
+        log_path = f'{folder}/{args.algo}({args.model})_{args.data}.log'
+        logging.basicConfig(
+            level=logging.DEBUG, filename=log_path, filemode='w', format=FORMAT)
 
-    print(f'data_path = {data_path}')
-    logging.info(f'data_path = {data_path}')
+        print(
+            f'Algo: {args.algo}, Model: {args.model}, Data: {args.data}, scoring: {args.scoring}, k_fold: {args.k_fold}, confusion_m: {args.confusion_m}')
+        logging.info(
+            f'Algo: {args.algo}, Model: {args.model}, Data: {args.data}, scoring: {args.scoring}, k_fold: {args.k_fold}, confusion_m: {args.confusion_m}')
 
-    print(f'data:{len(dataset)}, y={target} , X_num = {len(X.columns)}')
-    logging.info(
-        f'data:{len(dataset)}, y={target} , X_num = {len(X.columns)}')
+        print(f'data_path = {data_path}')
+        logging.info(f'data_path = {data_path}')
 
-    print(f'{args.model} config: {model_cfg}')
-    logging.info(f'{args.model} config: {model_cfg}')
+        print(f'data:{len(dataset)}, y={target} , X_num = {len(X.columns)}')
+        logging.info(
+            f'data:{len(dataset)}, y={target} , X_num = {len(X.columns)}')
 
-    hyper(args, model_cfg, X, y, file, folder, task_type)
+        print(f'{args.model} config: {model_cfg}')
+        logging.info(f'{args.model} config: {model_cfg}')
+
+        hyper(args, model_cfg, X, y, file, folder, task_type)
